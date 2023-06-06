d11548235ba992f06bff15ee39e0721d31a4b762
Aldo2303 <aldod03@gmail.com>
2023-06-06 06:35:59 -0700

    task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-        new_copy = ""
-        for i in my_string:
-            if i != 'c' and i != 'C':
-                new_copy += i
-        return (new_copy)
+    new_copy = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_copy += i
+    return (new_copy)
