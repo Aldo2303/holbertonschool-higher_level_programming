a90771a9edcab1aa23d31ca9d3abc3494777d751
Aldo2303 <aldod03@gmail.com>
2023-06-13 05:57:35 -0700

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """Create an instance of class Square"""
+    def __init__(self, size):
+        """
+        Initializes Square
+        Atributtes: size: size of square
+        """
+        self.__size = size
