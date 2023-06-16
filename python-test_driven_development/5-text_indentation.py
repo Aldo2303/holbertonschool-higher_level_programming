607a49503c2c57c5a5be1e534fb6cfc3ae5b87ec
Aldo2303 <aldod03@gmail.com>
2023-06-15 18:48:26 -0700

    task5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,7 +14,7 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    
+
     flag = 0
     no_print_space = ""
     for letter in range(len(text)):
