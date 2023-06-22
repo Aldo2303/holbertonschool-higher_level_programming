203fb81cf6e780f1a65cb153a1b8872814f0c2fb
Aldo2303 <aldod03@gmail.com>
2023-06-22 08:23:09 -0700

    prototype empty of area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry"""
+
+
+class BaseGeometry:
+    """
+    Public method: area
+    """
+    def area(self):
+        raise Exception ("area() is not implemented")
