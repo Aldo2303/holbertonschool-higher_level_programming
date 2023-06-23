c57702e6588ad41017038a35f2c66e2113d75b3b
Aldo2303 <aldod03@gmail.com>
2023-06-23 13:26:48 -0700

    now import of method base geometry, because 1 check is failure

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,29 +1,7 @@
 #!/usr/bin/python3
 """Module 8-rectangle
-I can import the geometric base class in the following way:
+I can import the geometric base class in the following way:"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-
-
-class BaseGeometry:
-    """
-    Public method: area
-    Public method: integer_validator
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        method that check if value is an int and is >0
-        arguments:
-            name: is always a string
-            value
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
