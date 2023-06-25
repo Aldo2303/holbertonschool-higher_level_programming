9e302265c18de219d84b042d61689f40a01b6f03
Aldo2303 <aldod03@gmail.com>
2023-06-25 06:46:29 -0700

    read file, use function read() for print

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 0-read_file"""
+
+def read_file(filename=""):
+    """
+    function that reads a text file (UTF8)
+    and prints it to stdout:
+    argument:
+        filename: file to be read
+    """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        print(file.read(), end="")
