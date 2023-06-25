fdca3af747edfac74550dd20ed731e3339a90ce6
Aldo2303 <aldod03@gmail.com>
2023-06-25 07:17:40 -0700

    function that transformer string in format JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object
+    argument:
+        my_obj
+    Return:
+        the JSON representation of an object(string)
+    """
+    return (json.dumps(my_obj))
