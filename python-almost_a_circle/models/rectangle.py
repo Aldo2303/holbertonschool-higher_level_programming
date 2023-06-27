9252f6753a0168db258f2e304504613a63bdd962
Aldo2303 <aldod03@gmail.com>
2023-06-27 12:59:04 -0700

    update the type error and value error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -70,6 +70,10 @@ class Rectangle(Base):
             width of rectangle
         """
         self.__width = width
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
 
     @height.setter
     def height(self, height):
@@ -79,6 +83,10 @@ class Rectangle(Base):
             height of rectangle
         """
         self.__height = height
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
 
     @x.setter
     def x(self, x):
@@ -88,6 +96,10 @@ class Rectangle(Base):
             x for rectangle
         """
         self.__x = x
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
 
     @y.setter
     def y(self, y):
@@ -97,3 +109,7 @@ class Rectangle(Base):
             y for rectangle
         """
         self.__y = y
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
