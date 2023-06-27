a55e87c4c08e45d1d1e147c903fc4236e9e4c296
Aldo2303 <aldod03@gmail.com>
2023-06-27 13:51:09 -0700

    test check the type and value

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,11 +10,6 @@ class Test_Rectangle(unittest.TestCase):
     """unittests for the functions in class Rectangle"""
 
     def test_int(self):
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 2)
-
-    def test_int(self):
         r2 = Rectangle(2, 10)
         self.assertEqual(r2.width, 2)
         self.assertEqual(r2.height, 10)
@@ -27,10 +22,29 @@ class Test_Rectangle(unittest.TestCase):
         self.assertEqual(r3.y, 0)
         self.assertEqual(r3.id, 12)
 
-    def test_empty(self):
-        r4 = Rectangle(None, None)
-        self.assertEqual(r4.width, None)
-        self.assertEqual(r4.height, None)
+    def test_Type_Arg(self):
+        with self.assertRaises(TypeError):
+            Rectangle("10", 2, 0, 0, 12)
+        with self.assertRaises(TypeError):
+            Rectangle(10, [2, 5], 0, 0, 12)
+        with self.assertRaises(TypeError):
+            Rectangle(10, 2, 0, {"k": 0}, 12)
+        with self.assertRaises(TypeError):
+            Rectangle((10, 7), 2, 0, 0, 12)
+        with self.assertRaises(TypeError):
+            Rectangle(10, 2, 0, "hello", 12)
+        with self.assertRaises(TypeError):
+            Rectangle(10, 2, 0, {0}, 12)
+
+    def test_negative_and_Zero(self):
+        with self.assertRaises(ValueError):
+            Rectangle(0, 2, 0, 0, 12)
+        with self.assertRaises(ValueError):
+            Rectangle(10, -2, 0, 0, 12)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 2, -4, 0, 12)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 2, 0, -4, 12)
 
 
 if __name__ == "__main__":
