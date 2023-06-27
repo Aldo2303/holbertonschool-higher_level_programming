4227f764303084b4e1729e79663a0f883fb87064
Aldo2303 <aldod03@gmail.com>
2023-06-27 09:00:46 -0700

    first test

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Unittest for class Base
+"""
+import unittest
+from models.base import Base
+
+class TestMaxBase(unittest.TestCase):
+    """unittests for the functions in class Base"""
+
+    def test_int_id(self):
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
+
+    def test_empty_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
+
+    def test_default_id(self):
+        b1 = Base(None)
+        self.assertEqual(b1.id, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
