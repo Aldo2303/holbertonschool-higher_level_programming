e686bf333cdad5e77cbceab0feaaa57b2e307d3f
Aldo2303 <aldod03@gmail.com>
2023-06-29 07:40:34 -0700

    building a new class square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module 3-class_square
+"""
+from models.rectangle import Rectangle
+"""
+import class Rectangle
+"""
+
+
+class Square(Rectangle):
+    """
+     inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        class contructor of square
+        arg:
+            size (size for width and size for height)
+            x
+            y
+            id
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Update the class square
+        overriding the __str__ method
+        Return:
+            [Square] (<id>) <x>/<y> - <size>
+        """
+        return ("[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.width))
