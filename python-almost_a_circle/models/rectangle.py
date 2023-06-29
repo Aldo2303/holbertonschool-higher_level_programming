ea65a2527ded011b5a45e65928bfa148f08a63d1
Aldo2303 <aldod03@gmail.com>
2023-06-29 06:34:38 -0700

    method update, used the *args and *kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -128,10 +128,10 @@ class Rectangle(Base):
         public method: def display(self)
         that prints in stdout the Rectangle instance with the character #
         """
-        for a in range(self.y):
+        for a in range(self.__y):
             print()
         for column in range(self.__height):
-            for b in range(self.x):
+            for b in range(self.__x):
                 print(" ", end="")
             for row in range(self.__width):
                 print("#", end="")
@@ -148,19 +148,32 @@ class Rectangle(Base):
             self.__class__.__name__, self.id, self.__x, self.__y,
             self.__width, self.__height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
         Public method: def update(self, *args)
         that assigns an argument to each attribute
         arg: list of arguments
         """
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) == 2:
-            self.width = args[1]
-        if len(args) == 3:
-            self.height = args[2]
-        if len(args) == 4:
-            self.x = args[3]
-        if len(args) == 5:
-            self.y = args[4]
+        if (args):
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.__width = args[1]
+            if len(args) == 3:
+                self.__height = args[2]
+            if len(args) == 4:
+                self.__x = args[3]
+            if len(args) == 5:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
