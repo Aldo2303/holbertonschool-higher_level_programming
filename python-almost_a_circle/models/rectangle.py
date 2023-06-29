ca090beeba819a47df68f0e646694ce82e6004ae
Aldo2303 <aldod03@gmail.com>
2023-06-29 12:26:14 -0700

    added method dictionary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -154,16 +154,15 @@ class Rectangle(Base):
         that assigns an argument to each attribute
         arg: list of arguments
         """
-        if (args):
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) == 2:
+        if len(args) >= 1:
+            self.id = args[0]
+            if len(args) >= 2:
                 self.__width = args[1]
-            if len(args) == 3:
+            if len(args) >= 3:
                 self.__height = args[2]
-            if len(args) == 4:
+            if len(args) >= 4:
                 self.__x = args[3]
-            if len(args) == 5:
+            if len(args) >= 5:
                 self.__y = args[4]
         else:
             for key, value in kwargs.items():
@@ -177,3 +176,17 @@ class Rectangle(Base):
                     self.__x = value
                 if key == "y":
                     self.__y = value
+
+    def to_dictionary(self):
+        """
+        Public method: def to_dictionary(self)
+        Return:
+            the dictionary representation of a Rectangle
+        """
+        dictionary = {}
+        dictionary["id"] = self.id
+        dictionary["width"] = self.__width
+        dictionary["height"] = self.__height
+        dictionary["x"] = self.__x
+        dictionary["y"] = self.__y
+        return (dictionary)
