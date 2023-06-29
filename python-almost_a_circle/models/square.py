b91b827313ab2da6943ef84b69c16946193326e9
Aldo2303 <aldod03@gmail.com>
2023-06-29 07:48:05 -0700

    getter and setter the size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -33,7 +33,7 @@ class Square(Rectangle):
         return ("[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
             self.__class__.__name__, self.id, self.x, self.y,
             self.width))
-    
+
     @property
     def size(self):
         """
